051c647cfeec04961e646bc555a22cbe7273efd4
kinchiller <kingchiller@gmail.com>
2022-03-16 21:33:31 +0100

    Add plausible

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const { withPlausibleProxy } = require('next-plausible')
+const withPlugins = require('next-compose-plugins')
+
+const plugins = [withPlausibleProxy]
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPlugins(plugins, nextConfig)
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import PlausibleProvider from 'next-plausible'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="sharrr.com">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
